d612e425761c6c34ca6ebc30e61fb3fdd20df638
katsuraku <kirsten.jones@ft.com>
2016-03-30 18:10:48 +0100

    First Voting component that renders current pair of entries (hard-coded for now).

--- src/index.js
+++
@@ -1,2 +0,0 @@
-// This file is the app's entry point file.
-console.log('Yoyoyo');
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+// This file is the app's entry point file.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} />,
+  document.getElementById('app')
+);
