eb18c1abe587b5fe9435367704b252367cccd287
katsuraku <kirsten.jones@ft.com>
2016-04-04 19:30:22 +0100

    Voting component buttons onClick connects to core vote function. How is it connected using this.props.vote?

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { renderIntoDocument, scryRenderedDOMComponentsWithTag } from 'react-addons-test-utils';
+import { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } from 'react-addons-test-utils';
 import { expect } from 'chai';
 import Voting from '../../src/components/Voting';
 
@@ -14,5 +14,19 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Trainspotting');
     expect(buttons[1].textContent).to.equal('28 Days Later');
+  });
+
+  it('invokes a callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={['Trainspotting', '28 Days Later']}
+              vote={vote}/>
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
   });
 });
