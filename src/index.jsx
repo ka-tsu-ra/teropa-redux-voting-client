309de2e0c8a4f370917079cff7964a1b502782e5
katsuraku <kirsten.jones@ft.com>
2016-04-07 20:46:06 +0100

    Adds a results route to show another view - the results page

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
+import App from './components/App.jsx';
 import Voting from './components/Voting.jsx';
+import Results from './components/Results.jsx';
 
 // Declaratively define a routing table using the React component called Route.
 // Point to the App component for the root Route in the config, which will be shared for 
 // all the concrete routes within. App component purpose is to rener all markup that is common 
 // accross all routes.
- 
+// Specify that for path named '/results', the Results component should be used 
+
 const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
   <Route path="/" component={Voting} />
 </Route>;
 
